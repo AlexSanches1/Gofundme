dc4e4308be2a8a8584550972c950dc0775d2d15b
sanch <sanches2026@gmail.com>
2018-10-16 12:04:00 +0300

    //

---
+++ src/main/java/com/app/gofundme/GofundmeApplication.java
@@ -0,0 +1,12 @@
+package com.app.gofundme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofundmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofundmeApplication.class, args);
+	}
+}
